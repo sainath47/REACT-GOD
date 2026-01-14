3963b15aad9af50209a4e2988094de90a70585c8
sainath47 <reddysainath47@gmail.com>
2026-01-14 15:54:47 +0530

    done with routing

--- claude-train/src/App.test.tsx
+++ claude-train/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './last/App';
+import App from './pages/last';
 
 test('renders learn react link', () => {
   render(<App />);
